00e51e9ba86114dde153c2bfd1cba6c1a089ff4a
tobiasvalbjoern <tvalbjoern@gmail.com>
2018-11-01 14:29:46 +0100

    First commit

---
+++ app/src/main/java/dk/e5pme/challenge4/MainActivity.kt
@@ -0,0 +1,12 @@
+package dk.e5pme.challenge4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
